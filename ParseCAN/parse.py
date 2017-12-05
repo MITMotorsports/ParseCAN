e236ff364eed6fd79307ee14f41af211ed3d1253
Nick Stathas <nickstathas@outlook.com>
2017-12-05 09:06:11 -0500

    All (useful) SI prefixes

--- ParseCAN/parse.py
+++ ParseCAN/parse.py
@@ -12,14 +12,19 @@ def SI(value, expunit):
         return value
 
     SI_MOD = {
+        'p': 1e-12,
+        'n': 1e-9,
         'u': 1e-6,
         'm': 1e-3,
         'c': 1e-2,
         'd': 1e-1,
         '': 1,
+        'da': 1e1,
+        'h': 1e2,
         'k': 1e3,
-        'K': 1e3,
         'M': 1e6,
+        'G': 1e9,
+        'T': 1e12,
     }
 
     match = re.search(r'(\d\.*\d*)*([A-Za-z]*)', value)
