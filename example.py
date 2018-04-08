82b6bf562bb2a42cb33b54b3783433783c4dab7d
Nick Stathas <nickstathas@outlook.com>
2018-04-07 20:07:41 -0400

    Better example

--- example.py
+++ example.py
@@ -13,16 +13,27 @@ def msg_conv(msg):
     return data.message(can_id=msg.arbitration_id, data=bitstr(msg.data))
 
 
+black_list = [0xD8, 0xF0]
+black_list.extend(range(0x521, 0x525))
+print('Ignoring the following: ', black_list)
+
+white_list = []
+raw_list = [0xD4]
+
 try:
     for msg in bus:
-        if msg.arbitration_id == 218:
-            msg.arbitration_id = 217
-            print('VCU FAKE')
+        can_id = msg.arbitration_id
+        if can_id in black_list or can_id not in white_list:
+            continue
 
         print(hex(msg.arbitration_id), hex(msg_conv(msg).data))
 
+        if msg.arbitration_id in raw_list:
+            continue  # raw
         try:
-            print(car.unpack(msg_conv(msg))['can0'])
+            msg = car.unpack(msg_conv(msg))['can0']
+            print(msg)
+
         except KeyError:
             print('Unknown message.')
 
