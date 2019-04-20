c5833baf7e2a63a70adc1c36a5ae8a13b20cbe82
barryxu2 <33433326+barryxu2@users.noreply.github.com>
2019-04-20 17:02:53 -0400

    Unpack logs using FrameTimedBus

--- ParseCAN/data/log.py
+++ ParseCAN/data/log.py
@@ -3,37 +3,27 @@ import ParseCAN as pcn
 
 class Log:
     def unpack(self, spec, include_raw=False, **kwargs):
-        bus_map = {}
         bus_unique = spec.protocol['name']['can'].bus
-        for bus_nm in bus_unique['name']:
-            frame_unique = bus_unique['name'][bus_nm].frame
-            for id in frame_unique['key']:
-                bus_map[id] = bus_unique['name'][bus_nm]
-
         for t_frame in self:
             # Temporary -- working with incomplete spec.
             # Should remove check and throw error
 
-            if t_frame.id in bus_map:
-                bus = bus_map[t_frame.id]
+            # Probably better to implement unpack on data.Frame
+            unp = bus_unique['name'][t_frame.bus].unpack(t_frame, **kwargs)
+            msg_nm = [t_frame.bus]
+            while isinstance(unp, tuple):
+                msg_nm.append(unp[0].name)
+                unp = unp[1]
 
-                unp = bus_map[t_frame.id].unpack(t_frame, **kwargs)
-                msg_nm = [bus.name]
-                while isinstance(unp, tuple):
-                    msg_nm.append(unp[0].name)
-                    unp = unp[1]
+            msg_nm = '.'.join(msg_nm)
 
-                msg_nm = '.'.join(msg_nm)
-
-                if include_raw:
-                    yield (msg_nm,
-                           unp,
-                           t_frame)
-                else:
-                    yield (msg_nm,
-                           unp)
+            if include_raw:
+                yield (msg_nm,
+                       unp,
+                       t_frame)
             else:
-                print('id {} not in spec'.format(t_frame.id))
+                yield (msg_nm,
+                       unp)
 
 
 class File(Log):
