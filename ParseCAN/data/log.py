f5ce0f2ba77ca6e18b0b977a1c3638409baa70e0
barryxu2 <33433326+barryxu2@users.noreply.github.com>
2019-04-20 17:29:49 -0400

    Restore frame id check for testing

--- ParseCAN/data/log.py
+++ ParseCAN/data/log.py
@@ -3,27 +3,28 @@ import ParseCAN as pcn
 
 class Log:
     def unpack(self, spec, include_raw=False, **kwargs):
-        bus_unique = spec.protocol['name']['can'].bus
         for t_frame in self:
-            # Temporary -- working with incomplete spec.
-            # Should remove check and throw error
+            # TEMPORARY. Testing with incomplete spec!
+            # Remove check and throw error in final version!
+            if t_frame.id in t_frame.bus.frame['key']:
+                # Could just implement unpack in data.Frame
+                unp = t_frame.bus.unpack(t_frame, **kwargs)
+                msg_nm = [t_frame.bus.name]
+                while isinstance(unp, tuple):
+                    msg_nm.append(unp[0].name)
+                    unp = unp[1]
 
-            # Probably better to implement unpack on data.Frame
-            unp = bus_unique['name'][t_frame.bus].unpack(t_frame, **kwargs)
-            msg_nm = [t_frame.bus]
-            while isinstance(unp, tuple):
-                msg_nm.append(unp[0].name)
-                unp = unp[1]
+                msg_nm = '.'.join(msg_nm)
 
-            msg_nm = '.'.join(msg_nm)
-
-            if include_raw:
-                yield (msg_nm,
-                       unp,
-                       t_frame)
-            else:
-                yield (msg_nm,
-                       unp)
+                if include_raw:
+                    yield (msg_nm,
+                           unp,
+                           t_frame)
+                else:
+                    yield (msg_nm,
+                           unp)
+            # else:
+            #     print('id {} not in spec'.format(t_frame.id))
 
 
 class File(Log):
