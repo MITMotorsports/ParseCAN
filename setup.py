2ab035a0f5a3a420d24d7c6ec0a64c4241da5820
Renggeng Zheng <rzhengcontacts@gmail.com>
2024-09-02 18:39:07 -0400

    reqs copied over from CANlib and modified for task

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MotoParseCAN",
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "dataclasses",
+        "intervaltree",
+        "PyYAML",
+        "pint",
+        "numpy",
+
+    ],
+)
