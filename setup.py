79df33213cd02b4577e6a153a1029dca6d01cf54
Renggeng Zheng <rzhengcontacts@gmail.com>
2024-09-07 11:39:21 -0400

    compiles

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="MotoParseCAN",
     version="1.0.0",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         "dataclasses",
